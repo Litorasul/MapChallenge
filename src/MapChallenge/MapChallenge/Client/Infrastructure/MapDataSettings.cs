ee27e4bfa143f9ab778024fd491f23b60942807b
Nikolay Kelov <n_kelov@yahoo.com>
2020-06-08 08:37:50 +0200

    Add more game infrastructure - MapDataSettings, GamePlay page and constants with paths to GeoJSON maps.

---
+++ src/MapChallenge/MapChallenge/Client/Infrastructure/MapDataSettings.cs
@@ -0,0 +1,23 @@
+﻿namespace MapChallenge.Client.Infrastructure
+{
+    /// <summary>
+    /// Configuration class for the maps component. Naming the properties according to what the Syncfusion Maps are looking for.
+    /// </summary>
+    public class MapDataSettings
+    {
+        /// <summary>
+        /// Is the map async.
+        /// </summary>
+        public bool async { get; set; }
+
+        /// <summary>
+        /// The path to the GeoJSON file.
+        /// </summary>
+        public string dataOptions { get; set; }
+
+        /// <summary>
+        /// HTTP Method.
+        /// </summary>
+        public string type { get; set; }
+    }
+}
--- src/MapChallenge/MapChallenge/Shared/GlobalConstants.cs
+++ src/MapChallenge/MapChallenge/Shared/GlobalConstants.cs
@@ -8,14 +8,30 @@
 
         public const string JsonContentType = "application/json";
 
+        // For Seeding Data.
         public const string CountriesAndCapitalsJsonPath = @"../Server/Data/Seeding/JsonData/country-capital-continent.json";
 
         public const string UsStatesAndCapitalsJsonPath = @"../Server/Data/Seeding/JsonData/us_state_capitals.json";
 
         public const string CanadaStatesAndCapitalsJsonPath = @"../Server/Data/Seeding/JsonData/canadian-provinces.json";
 
+        // Game Elements
         public const int ElementsAmountInShortGame = 20;
 
+        // Client GeoJSON paths
+        public const string UsaMapPath = ".../MapsGeoJson/usa.json";
+
+        public const string CanadaMapPath = ".../MapsGeoJson/canada.geo.json";
+
+        public const string AfricaMapPath = ".../MapsGeoJson/AfricaLow.json";
+
+        public const string AsiaMapPath = ".../MapsGeoJson/asia.geo.json";
+
+        public const string EuropeMapPath = ".../MapsGeoJson/europe.geo.json";
+
+        public const string SouthAmericaMapPath = ".../MapsGeoJson/SouthAmericaLow.json";
+
+        // List of all continent names
         public static readonly string[] Continents =
         {
             "Africa",
